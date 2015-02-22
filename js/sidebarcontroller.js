7580b017690ae6a93013c5a7ac1934a358a38592
mame0112 <mame0112@gmail.com>
2015-02-22 12:33:10 +0900

    Change to get sidebar data from Controller

--- js/sidebarcontroller.js
+++ js/sidebarcontroller.js
@@ -2,12 +2,23 @@ wisdomApp.controller('SidebarController', ['$scope', 'Constants', function($scop
  	console.log("SidebarController");
  	console.log(Constants.planetName);
 
+ 	$scope.items = [
+ 		{id: 1, name: "test1"},
+ 		{id: 2, name: "test2"},
+ 		{id: 3, name: "test3"},
+ 	];
+
  	$scope.initialize = function() {
 		console.log("initialize");
+		setupItems();
  	};
 
  	$scope.isVisible = function() {
 		console.log("isVisible");
+ 	};
+
+ 	var setupItems = function(){
+ 		console.log("setupItems");
  	};
 
 }]);
