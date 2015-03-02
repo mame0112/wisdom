3165cbb9ab9db0f977b5b30539376ae3d45c6bf4
mame0112 <mame0112@gmail.com>
2015-03-03 01:54:29 +0900

    Change apiService

--- js/util/apiServices.js
+++ js/util/apiServices.js
@@ -1,10 +1,25 @@
-wisdomApp.factory('apiService', ['$resource',
-  function($resource) {
-    return $resource('api/wisdom/:id', {}, {
-      // Get certain or all category
-      get_category: {method: 'GET', isArray: false},
-      // Add as POST
-      save: {method: 'POST'}
-    });
-  }
+wisdomApp.factory('apiService', ['$resource', function($resource) {
+	return $resource('api/wisdom/:id', 
+		{servlet_resp_id: 1, twitter_name: 'TwitterName'},
+		 {query: {method: 'GET', isArray: false}
+		});
+	}
 ]);
+    // return $resource('api/wisdom/:id', {}, {
+    //   // Get certain or all category
+    //   get_category: {method: 'GET', isArray: false},
+    //   // Add as POST
+    //   save: {method: 'POST'}
+    // });
+
+
+// wisdomApp.factory('apiService', ['$resource',
+//   function($resource) {
+//     return $resource('api/wisdom/:id', {}, {
+//       // Get certain or all category
+//       get_category: {method: 'GET', isArray: false},
+//       // Add as POST
+//       save: {method: 'POST'}
+//     });
+//   }
+// ]);
